b0f588897011de33de3f9da63e661a318bc6fd3c
ayaze01 <87978229+ayaze01@users.noreply.github.com>
2022-12-24 19:08:05 +0700

    i

---
+++ mm/#bash-obfuscate.sh
@@ -0,0 +1,28 @@
+#bash-obfuscate
+bash-obfuscate add-ssh -o add-ssh
+bash-obfuscate add-ws -o add-ws
+bash-obfuscate add-vless -o add-vless
+bash-obfuscate add-tr -o add-tr
+bash-obfuscate add-ss -o add-ss
+
+bash-obfuscate cek-ssh -o cek-ssh
+bash-obfuscate cek-ws -o cek-ws
+bash-obfuscate cek-vless -o cek-vless
+bash-obfuscate cek-tr -o cek-tr
+bash-obfuscate cek-ss -o cek-ss
+
+bash-obfuscate del-ssh -o del-ssh
+bash-obfuscate del-ws -o del-ws
+bash-obfuscate del-vless -o del-vless
+bash-obfuscate del-tr -o del-tr
+bash-obfuscate del-ss -o del-ss
+
+bash-obfuscate renew-ssh -o renew-ssh
+bash-obfuscate renew-ws -o renew-ws
+bash-obfuscate renew-vless -o renew-vless
+bash-obfuscate renew-tr -o renew-tr
+bash-obfuscate renew-ss -o renew-ss
+
+bash-obfuscate get-domain -o get-domain
+
+bash-obfuscate update -o update
---
+++ mm/#shc.sh
@@ -0,0 +1,30 @@
+#shc
+#shc -rf file -o file
+
+shc -rf add-ssh -o add-ssh
+shc -rf add-ws -o add-ws
+shc -rf add-vless -o add-vless
+shc -rf add-tr -o add-tr
+shc -rf add-ss -o add-ss
+
+shc -rf cek-ssh -o cek-ssh
+shc -rf cek-ws -o cek-ws
+shc -rf cek-vless -o cek-vless
+shc -rf cek-tr -o cek-tr
+shc -rf cek-ss -o cek-ss
+
+shc -rf del-ssh -o del-ssh
+shc -rf del-ws -o del-ws
+shc -rf del-vless -o del-vless
+shc -rf del-tr -o del-tr
+shc -rf del-ss -o del-ss
+
+shc -rf renew-ssh -o renew-ssh
+shc -rf renew-ws -o renew-ws
+shc -rf renew-vless -o renew-vless
+shc -rf renew-tr -o renew-tr
+shc -rf renew-ss -o renew-ss
+
+shc -rf get-domain -o get-domain
+
+shc -rf update -o update
