b4aa89472882f24123f3c9aa144ff1da820d9e65
ayaze01 <87978229+ayaze01@users.noreply.github.com>
2022-12-26 05:54:34 +0700

    Update 611.sh

--- mm/611.sh
+++ mm/611.sh
@@ -5,8 +5,7 @@ cd
 wget -O 611.deb https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/6.1.1-bbrplus/Debian-Ubuntu_Required_linux-image-6.1.1-bbrplus_6.1.1-bbrplus-1_amd64.deb
 chmod +x 611.deb
 dpkg -i 611.deb
-rm -f 611.deb
-rm -f 611.ssh
+rm -f 611*
 echo '
 net.ipv4.tcp_congestion_control = bbrplus
 net.core.default_qdisc = fq' >>/etc/sysctl.conf
